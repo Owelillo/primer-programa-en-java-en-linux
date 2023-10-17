fc86ea1088b01de9aff883c6ce78af39ea773472
Gabriel <owelcontacto@gmail.com>
2023-10-17 12:14:03 +0200

    Añado mi lugar favorito

--- prueba.java
+++ prueba.java
@@ -3,6 +3,7 @@ public class prueba {
 	public static void main(String args[]){
 
 		System.out.println("Deporte favorito: Tenis de mesa");
+		System.out.println("Llugar favorito: El parque");
 		
 
 	}
